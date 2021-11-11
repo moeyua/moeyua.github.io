e371347e1f68481ecefb8ec2594b63b30c14ed2c
moeyua <moeyua@foxmail.com>
2021-11-11 15:48:03 +0800

    update config

--- backup.sh
+++ backup.sh
@@ -2,15 +2,3 @@
 cp themes/suka/_config.yml _config.suka.yml
 
 echo "backup success"
-
-git add -A
-
-echo "add success"
-
-git commit -m " backup config "
-
-echo "commit success"
-
-git push
-
-echo "push success"
